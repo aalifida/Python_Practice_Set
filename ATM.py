0bf271103b6ad472e9f527ac4805d79b19db97e3
Aali Fida <waleed.fida2@gmail.com>
2024-06-29 19:24:19 +0500

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,11 +1,22 @@
-class Atm():
+class Atm:
     def __init__(self):
-        self.balance = 0
-        self.pin = "1223"
-        self.menu()
+        self.__balance = 0
+        self.__pin = "1223"
 
-    def menu(self):
-        while True:
+
+    def get_pin(self):
+        return self.__pin
+    
+
+    def set_pin(self,newPin):
+        if(type(newPin)==str and len(newPin)== 4):
+            self.__pin=newPin
+            print("Pin Changed")
+        else:
+            print("Invalid Pin")
+    
+
+    def __menu(self):
             userInput = input("""
              How would you like to proceed??
              For Pin Setting Press 1
@@ -24,29 +35,28 @@ class Atm():
                 self.view_balance()
             elif userInput == "5":
                 print("Good Bye")
-                break
             else:
                 print("Invalid Option, Please Try Again")
 
     def create_pin(self):
-        self.pin = input("Enter Your New Pin: ")
+        self.__pin = input("Enter Your New Pin: ")
         print("Pin Created Successfully")
 
     def deposit_balance(self):
         user_enteredpin = input("Please Enter Your 4 digit pin: ")
-        if user_enteredpin == self.pin:
+        if user_enteredpin == self.__pin:
             amount = int(input("Enter amount of money you want to deposit: "))
-            self.balance += amount
+            self.__balance += amount
             print("Deposit Successful")
         else:
             print("Incorrect Pin")
 
     def withdraw_balance(self):
         user_enteredpin = input("Please Enter Your 4 digit pin: ")
-        if user_enteredpin == self.pin:
+        if user_enteredpin == self.__pin:
             amount = int(input("Enter amount of money you want to Withdraw: "))
-            if amount <= self.balance:
-                self.balance -= amount
+            if amount <= self.__balance:
+                self.__balance -= amount
                 print("Withdraw Successful")
             else:
                 print("Insufficient Balance")
@@ -55,10 +65,12 @@ class Atm():
 
     def view_balance(self):
         user_enteredpin = input("Please Enter Your 4 digit pin: ")
-        if user_enteredpin == self.pin:
-            print(f"Your balance is: {self.balance}")
+        if user_enteredpin == self.__pin:
+            print(f"Your balance is: {self.__balance}")
         else:
             print("Invalid Pin")
 
 # Create an instance of the ATM class
 aali = Atm()
+aali.set_pin("2093")
+print(aali.get_pin())
